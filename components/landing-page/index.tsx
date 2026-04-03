d50c48a1b7ec9b42df3f536bc68e834d22137a63
Justin Lauricio <lauriciolau902@gmail.com>
2026-04-04 00:00:14 +0800

    chore(UI): added navigation-menu and collapsible components

--- components/landing-page/index.tsx
+++ components/landing-page/index.tsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <Header />
         <div className="items-center text-center">
-          <h1 className="text-4xl font-bold">Welcome</h1>
+          <h1 className="text-4xl font-bold">This is a landing page</h1>
         </div>
       </div>
     </main>
