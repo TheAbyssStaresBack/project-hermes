3a1a390956efcf5fab0ec8349386d4e28ec230ac
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-04-03 10:19:25 +0800

    fix: homepage redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-'use client';
-
-import ControlCenter from '@/components/control-center';
 import LandingPage from '@/components/landing-page';
-import { useUser } from '@/hooks/useUser';
 
 export default function Home() {
-  const user = useUser();
-  // Loading user session...
-  if (user === undefined) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <p className="text-lg text-foreground/70">Loading...</p>
-      </div>
-    );
-  }
-  // If user exists, show control center
-  if (user) {
-    return <ControlCenter />;
-  }
-  // If no user, show landing page
   return <LandingPage />;
 }
