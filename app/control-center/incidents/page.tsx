ec9271d5286d70148a60be33cac53c9495d85a89
TheAbyssStaresBack <68632990+TheAbyssStaresBack@users.noreply.github.com>
2026-04-03 19:25:56 +0800

    chore: change directory of report-view files

--- app/control-center/incidents/page.tsx
+++ app/control-center/incidents/page.tsx
@@ -1,11 +1,13 @@
 import ChatBox from '@/components/control-center/incidents/chatbox';
 import { IncidentList } from '@/components/control-center/incidents/incident-list';
+import { ReportContainer } from '@/components/control-center/incidents/report-container';
 
 export default function Page() {
   return (
-    <div className="@container/main flex flex-1 flex-col gap-4 px-4 py-4 md:gap-6 md:py-6 lg:px-6 h-screen">
+    <div className="@container/main flex flex-1 flex-row gap-2 px-2 py-4 md:gap-2 md:py-6 lg:px-6 h-screen">
       <IncidentList />
       <ChatBox />
+      <ReportContainer />
     </div>
   );
 }
--- components/control-center/incidents/report-view/incident-list.tsx
+++ components/control-center/incidents/report-view/incident-list.tsx
@@ -5,11 +5,10 @@ import { Separator } from '@/components/ui/separator';
 import * as React from 'react';
 
 function getReportData() {
-  // function for grabbing report data from database
-  // TODO: replace with appropriate code before live deployment
+  // TODO: replace function content with database fetch
   // Each data must be a react component to modify chatbox.tsx
   const tags = Array.from({ length: 50 }).map(
-    (_, i, a) => `Sample Report number: ${a.length - i}`
+    () => `${Math.floor(Math.random() * 100000)}`
   );
   return tags;
 }
@@ -23,7 +22,7 @@ export function IncidentList() {
     // NOTE: refactor max-h to use relative measurements. might cause bugs in current state
     <ScrollArea className={`max-h-[calc(100vh-100px)] w-75 rounded-md border`}>
       <div className="p-4">
-        <h4 className="mb-4 text-sm leading-none font-medium">Tags</h4>
+        <h4 className="mb-4 text-sm leading-none font-medium">Reports</h4>
         {getReportData().map((tag) => (
           <React.Fragment key={tag}>
             <div className="text-sm">{tag}</div>
